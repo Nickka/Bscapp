7455bbcabbb7080bdecae78fffa65a4a6bc6b79e
Nicklas Nielsen <husblas@gmail.com>
2016-10-26 16:08:54 +0200

    v000.9

---
+++ app/src/main/java/com/example/thinknick/bscapp/SendActivity.java
@@ -0,0 +1,22 @@
+package com.example.thinknick.bscapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ThinkNick on 26-10-2016.
+ */
+
+public class SendActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_send);
+
+    }
+
+
+}
