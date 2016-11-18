fab7051801e8c83b8b3c642343f860bd3b72b2f9
Nicklas Nielsen <husblas@gmail.com>
2016-11-18 11:55:37 +0100

    v001.20 JSON og download billeder

--- app/src/main/java/com/example/thinknick/bscapp/Activities/RecievedSB.java
+++ app/src/main/java/com/example/thinknick/bscapp/Activities/RecievedSB.java
@@ -1,16 +1,26 @@
 package com.example.thinknick.bscapp.Activities;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.IBinder;
+import android.os.Messenger;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.thinknick.bscapp.R;
 import com.example.thinknick.bscapp.Service.CardService;
 import com.example.thinknick.bscapp.Service.DownloadPicService;
-import com.example.thinknick.bscapp.Service.FirebaseService;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +29,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import com.example.thinknick.bscapp.Service.DownloadPicService.MyBinder;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.example.thinknick.bscapp.R.id.imageView;
+import static com.example.thinknick.bscapp.R.id.recievedCardImageView;
+
+
 public class RecievedSB extends AppCompatActivity {
     private LoginActivity.UserLoginTask mAuthTask = null;
     private FirebaseAuth mAuth;
@@ -26,11 +48,21 @@ public class RecievedSB extends AppCompatActivity {
     private static final String TAG = "Login";
     private DatabaseReference mPostReference;
     private TextView recievedCardTextView;
+    private String path1;
+    private ImageView recievedCardImageView;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://bscapp-6b91c.appspot.com/");
+    /** Messenger for communicating with the service. */
+     DownloadPicService mService;
+
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mBound;
+    private File localFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_recieved_sb);
+        setContentView(R.layout.activity_recieved_sb);
         mPostReference = FirebaseDatabase.getInstance().getReference()
                 .child("users").child("YaMbfYZ3IDc19GX9XheMFPIoa8J3");
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -48,13 +80,13 @@ public class RecievedSB extends AppCompatActivity {
             }
         };
         recievedCardTextView = (TextView) findViewById(R.id.recievedCardTextView);
+        try {
+            getImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-
-        Intent intent = new Intent(this, DownloadPicService.class);
-       // intent.putExtra("FBservice", "DL");
-        this.startService(intent);
     }
-
     @Override
     public void onStart(){
         super.onStart();
@@ -69,6 +101,9 @@ public class RecievedSB extends AppCompatActivity {
                 CardService cid = dataSnapshot.getValue(CardService.class);
                 recievedCardTextView.setText(cid.email);
                 System.out.println(cid.email);
+               // if (mBound) {
+
+               // }
             }
 
             @Override
@@ -79,5 +114,70 @@ public class RecievedSB extends AppCompatActivity {
             }
         };
         mPostReference.addValueEventListener(postListener);
+
+        Intent intent = new Intent(this, DownloadPicService.class);
+        this.startService(intent);
+        this.bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
     }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(mConnection);
+    }
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the object we can use to
+            // interact with the service.  We are communicating with the
+            // service using a Messenger, so here we get a client-side
+            // representation of that from the raw IBinder object.
+            MyBinder myBinder = (MyBinder) service;
+            mService = myBinder.getService();
+
+            mBound = true;
+            //getPath();
+
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            mBound = false;
+        }
+    };
+
+    // Eneste måde vi kan få billedet i activityen, der kan ikke bruges en service til dette umiddelbart da eneste måde er at sætte stien til billedet, men stien bliver lavet og givet tilbage til activityen
+    // før at billedet er downloadet, hvilket bare giver et tomt element. Så backend kaldet er smidt i denne metode. It works... 6 timer spildt.
+    public void getImage() throws IOException{
+            StorageReference islandRef = storageRef.child("/userimages/magnushk@au.dk/magnushk@au.dk");
+            localFile = File.createTempFile("images", ".jpg");
+            path1 = localFile.getAbsolutePath();
+
+            islandRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Log.d(TAG, "worked buddy");
+                    Log.d(TAG, path1);
+                    File imgFile = new File(path1);
+                    if(imgFile.exists()){
+                        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                        ImageView myImage = (ImageView) findViewById(R.id.recievedCardImageView);
+
+                        myImage.setImageBitmap(myBitmap);
+                    }
+                }
+
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Log.d(TAG, "stmh went wrong");
+                }
+            });
+
+        }
 }
--- app/src/main/java/com/example/thinknick/bscapp/Service/DownloadPicService.java
+++ app/src/main/java/com/example/thinknick/bscapp/Service/DownloadPicService.java
@@ -2,12 +2,16 @@ package com.example.thinknick.bscapp.Service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.thinknick.bscapp.Activities.RecievedSB;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,19 +21,39 @@ import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.support.v7.widget.StaggeredGridLayoutManager.TAG;
+
+/*
+OUT OF SERVICE. BLIVER IKKE BRUGT PT.
+ */
 
 public class DownloadPicService extends Service {
     FirebaseStorage storage = FirebaseStorage.getInstance();
     StorageReference storageRef = storage.getReferenceFromUrl("gs://bscapp-6b91c.appspot.com/");
 
-    Bundle extras;
-    private static final String TAG = "Login";
+    private static final String TAG = "DLService";
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private String path1;
+    private IBinder mBinder = new MyBinder();
+    private String path2;
+    private File localFile;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v(TAG, "in onBind");
+        return mBinder;
+    }
+
     public DownloadPicService() {
         try {
-            getImageFromPath();
+            getImagePath2();
             Log.d(TAG, "Starter DL");
 
         } catch (IOException e) {
@@ -37,22 +61,31 @@ public class DownloadPicService extends Service {
         }
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 
     public void getImageFromPath() throws IOException {
-        StorageReference islandRef = storageRef.child("userimages/test@test.dk/test@test.dk");
+        StorageReference islandRef = storageRef.child("/userimages/test@test.dk/test@test.dk");
+        File directory = new File(Environment.getDataDirectory()
+                + "/Test2/");
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 
-        File localFile = File.createTempFile("images", "jpg");
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+
+        final File localFile = File.createTempFile(imageFileName, ".jpg", directory);
+        String path = localFile.getAbsolutePath();
+
+        System.out.println("test" + path);
+
         try {
             islandRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                     Toast.makeText(getApplicationContext(), "Downloaded", Toast.LENGTH_SHORT).show();
                     Log.d(TAG, "Downloaded til path");
+                    String mCurrentPhotoPath = "file:" + localFile.getAbsolutePath();
 
                 }
             }).addOnFailureListener(new OnFailureListener() {
@@ -68,5 +101,55 @@ public class DownloadPicService extends Service {
             System.out.println(e.getMessage());
         }
 
+    }
+
+    public String getImagePath2() throws IOException {
+        StorageReference islandRef = storageRef.child("/userimages/test@test.dk/test@test.dk");
+        localFile = File.createTempFile("images", ".jpg");
+        path1 = localFile.getAbsolutePath();
+
+        islandRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Log.d(TAG, "worked buddy");
+                Log.d(TAG, path1);
+              }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.d(TAG, "stmh went wrong");
+            }
+        });
+       
+
+        return path1;
+    }
+    public String getPath(){
+
+        return "hej";
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "in onUnbind");
+        return true;
+    }
+
+    public class MyBinder extends Binder {
+        public DownloadPicService getService() {
+            return DownloadPicService.this;
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "in onDestroy");
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.v(TAG, "in onRebind");
+        super.onRebind(intent);
     }
 }
